832fa98bed1cbe92a21208ff911ad5d4a2d5dc5c
latilanoh@gmail.com <latilanoh@gmail.com>
2018-08-06 13:54:30 -0500

    se busca implementar un for para colocar todas las habilidades del pokemon y hacer pruebas con el input-search

--- js/app.js
+++ js/app.js
@@ -7,21 +7,22 @@ $(document).ready(function(){
 $('#search-button').click(function() {
   swal("Ooops it's not working right now!", "Come back later!", "warning");
   
-  /*let inputPokemon = $('#search-input').val();
+  let inputPokemon = $('#search-input').val();
   console.log(inputPokemon);
   
-  let searchCard = document.querySelector("div.card");
-  //let searchCard = document.getElementsByName('');
+  //let searchCard = document.querySelector("div.card");
+  let searchCard = document.getElementsByName("");
   console.log(searchCard);
   
-  if (inputPokemon != searchCard ){
+  if (inputPokemon != searchCard ) {
     //$(searchCard).addClass('hidden');
     $(searchCard).css("color", "blue");
-  } else if {
-  //$(searchCard).removeClass('hidden');
-   $(searchCard).css("color", "green");
-   //swal("Houston we have a problem!", "Try typing again, please!", "warning");
-  }*/
+    } 
+    else  {
+    //$(searchCard).removeClass('hidden');
+    $(searchCard).css("color", "green");
+    //swal("Houston we have a problem!", "Try typing again, please!", "warning");
+  };
 
   $('#search-input').val('');
 
@@ -56,14 +57,30 @@ function profilePokemon(number) {
   })
   .done(function(response) {
     let ability = response.abilities[0].ability.name;
+    //console.log(ability);
+    //let abilityPokemon = "";
+    //let abilities = response.abilities;
+    //console.log(abilities);
+    
+    /*for (let abilityPokemon in abilities) {
+      console.log( `${abilities[abilityPokemon].ability.name}`);
+      //console.log(ability);
+    };*/
+
+    /*for (let i = 0; i < abilities.length; i++ ) {
+     let abilityPokemon = abilities[i].ability.name;
+      //console.log(abilityPokemon);
+      return abilityPokemon;
+    }*/
+
     let type = response.types[0].type.name;
     let weight = response.weight;
     let height = response.height;
-    let name = response.name;
+    let namePokemon = response.name;
        
-    let plantilla_pokemon = fillTemplate(number, name, ability, type, weight, height);
+    let plantilla_pokemon = fillTemplate(number, namePokemon, ability, type, weight, height);
       $('#section-pokemons').append(plantilla_pokemon);
-    searchPokemon(name);
+    //searchPokemon(namePokemon);
   })
   .fail(function() {
     //console.log("error cargando info de pokemons");
@@ -82,7 +99,7 @@ function fillTemplate(number, name, ability, type, weight, height) {
             <div class="card-reveal">
               <span class="card-title red-text text-darken-4"> ${name} <i class="material-icons right">close</i></span>
               <h6> Ability: </h6>
-              <li> ${ability} </li>
+              <li> ${abilityPokemon} ${ability} </li>
               <h6> Type: </h6>
               <li> ${type} </li>
               <h6> Weight / Heigth </h6>
@@ -91,6 +108,6 @@ function fillTemplate(number, name, ability, type, weight, height) {
           </div>`
 };
 
-function searchPokemon(namePokemon) {
+/*function searchPokemon(namePokemon) {
   return namePokemon;
-}
+}*/
